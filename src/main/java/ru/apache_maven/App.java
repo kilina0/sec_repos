c1440051da3f08e46622cd9770c5a536d3cae7c3
kilina0 <kilina1920@gmail.com>
2023-10-23 15:27:35 +0200

    Update App.java

--- src/main/java/ru/apache_maven/App.java
+++ src/main/java/ru/apache_maven/App.java
@@ -8,8 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        int i = 0;
-        int i = 0;
+        System.out.println(new AppBean("Maven").sayHello());
         System.out.println(new AppBean("Maven").sayHello());
         //add new row to test
         System.out.println( "It's me! Hi" );
