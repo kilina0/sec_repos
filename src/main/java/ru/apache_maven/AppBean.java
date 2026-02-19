484a0e92ad694d8672fd1b81d3174c876fc012d9
Ksenia Ilina <kilina1920@gmail.com>
2026-02-19 18:09:15 +0100

    Remove constructor from AppBean class
    Removed the constructor that initializes the text field.

--- src/main/java/ru/apache_maven/AppBean.java
+++ src/main/java/ru/apache_maven/AppBean.java
@@ -3,9 +3,6 @@ package ru.apache_maven;
 public class AppBean 
 {
 	private String text; 
-    public AppBean(final String text) {
-        this.text = text;
-    } 
     public String sayHello() {
         return "Hello " + text;
     } 
